5d1126d10d23222a5802ce0bfd45e996ca6141a3
Fabriziogianni7 <fabrizio@keyko.io>
2023-12-16 12:12:08 +0100

    remove comments

--- src/app.py
+++ src/app.py
@@ -3,31 +3,6 @@ from venv import logger
 import wave
 
 
-# don't do that, bacalhau jobs don't have access to interent
-# should get the file from the input folder
-# def download_from_ipfs_gateway(hash):
-#     url = f'https://ipfs.eth.aragon.network/ipfs/{hash}?filename=exitCode'
-
-#     print("URL:",url)
-
-#     output_dir = "./outputs"
-#     # input_dir = "/inputs"
-
-#     if not os.path.exists(output_dir):
-#         os.makedirs(output_dir)
-    
-#     response = requests.get(url)
-
-#     if response.status_code == 200:
-#         file_path = f'{output_dir}/downloaded_file_{hash}'
-#         with open(file_path, 'wb') as f:
-#             f.write(response.content)
-        
-#         print(f'File downloaded to: {file_path}')
-#     else:
-#         print('Error downloading file. Status code: {response.status_code}')
-
-
 input_dir = "/inputs"
 output_dir = "/outputs"
 
@@ -37,24 +12,11 @@ def main():
     print("file_path", file_path)
     
 
-
-    # opening a file and pasting to /outputs folder
-    # for file in os.listdir(input_dir):
-    #     file_path = os.path.join(input_dir, file)
-    #     print(f'Moving file: {file_path}')
-    #     if not os.path.exists(output_dir):
-    #         os.makedirs(output_dir)
-    #     destination_path = os.path.join(output_dir, file)
-    #     shutil.move(file_path, destination_path)
-    #     print(f'File moved to: {destination_path}')
-    #     logger.info(f'File moved to: {destination_path}')
-
     try:
         with wave.open(file_path, "rb") as wave_file:
-            print("in try")
             
+            #  audio frame and channels
             frame_rate = wave_file.getframerate()
-            print("in try")
             print(f"Frame rate of input audio: {frame_rate}")
             num_channels = wave_file.getnchannels()
             print(f"num_channels: {num_channels}")
